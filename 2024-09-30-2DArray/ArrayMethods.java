e4b722d53dc60aec6f14cd5976270f0ee17dea06
Alisherjon Turakulov <aturakulov100@gmail.com>
2024-09-30 23:32:10 -0400

    created test cases for arr2Dsum, wrote arr2DSum method, verified that it passed test cases, added another test case to arrToString

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -4,8 +4,19 @@ public class ArrayMethods{
     // Estella; eyee60@stuy.edu.
 
     // Test arrToString with 2D parameter
+	System.out.println("Tests for ArrToString---------------------");
     System.out.println(arrToString(new int[][] {{1, 2}, {5, 6}, {0, 3, 5}}));
     System.out.println(arrToString(new int[3][4]));
+	System.out.println(arrToString(new int[0][]));
+	System.out.println(arrToString(new int[4][0]));
+	System.out.println(arrToString(new int[3][3]));
+	System.out.println(arrToString(new int[][]{{1, 2, 3, 4}, {2, 65, 5}, {3, 6, 9, 12}, {4, 8, 432, 0, 6, 3, 6, 2}}));
+	System.out.println("Tests for ArrToString---------------------");
+	System.out.println(arr2DSum(new int[1][1])); // should be zero
+	System.out.println(arr2DSum(new int[][]{{1, 2, 3}, {1}, {2, 6, 2, 3, 5, 7}, {100}})); // should be 132
+	System.out.println(arr2DSum(new int[][]{{100, 200, 320}, {0}, {430, 10, 10, 10}, {435}})); //  1515
+	System.out.println(arr2DSum(new int[][]{{1, 0, 0}, {1, 0, 0}, {1, 0, 0}})); // 3
+	
 
 }
 
@@ -46,16 +57,22 @@ public class ArrayMethods{
       return ans += "]";
     }
 
-// /*Return the sum of all of the values in the 2D array */
-// public static int arr2DSum(int[][]nums){
-//   //use a nested loop to solve this
-// }
-//
-// /**Rotate an array by returning a new array with the rows and columns swapped.
-//   * You may assume the array is rectangular and neither rows nor cols is 0.
-//   * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
-//   */
-// public static int[][] swapRC(int[][]nums){
-//   return new int[1][1];
-// }
+ /*Return the sum of all of the values in the 2D array */
+public static int arr2DSum(int[][]nums){
+	int sum = 0;
+	for (int i = 0; i < nums.length; i++) {
+		for (int k = 0; k < nums[i].length; k++) {
+			sum += nums[i][k];
+		}
+	}
+	return sum;
+}
+
+ /**Rotate an array by returning a new array with the rows and columns swapped.
+   * You may assume the array is rectangular and neither rows nor cols is 0.
+   * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
+   */
+ public static int[][] swapRC(int[][]nums){
+   return new int[1][1];
+ }
 }
