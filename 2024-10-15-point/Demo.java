c5efe07078072a22730cc44da8e3b9eedfb7c788
Alisherjon Turakulov <aturakulov100@gmail.com>
2024-10-15 23:22:52 -0400

    create 3 points for equilateral triangle of length 1, print 3 side lengths, functions

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -15,6 +15,10 @@ public class Demo{
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
     Point p4 = new Point(p1);
+	//equilateral triangle points:
+	Point e1 = new Point(0, 0);
+	Point e2 = new Point(1, 0);
+	Point e3 = new Point(.5, Math.sqrt(1 - 0.25)); //using pythag teorem with 30-60-90 triangle
 	//print using the toString() method we defined in Point.java
     System.out.println(p3);
 	System.out.println(p4); //should be same as p1
@@ -28,5 +32,9 @@ public class Demo{
     System.out.println( p1.distanceTo(p2));
 	System.out.println( p1.distanceTo(p3));
 	System.out.println( p2.distanceTo(p3));
+	//Print Side lengths of equilat triangle (should be 1):
+	System.out.println(distance(e1, e2));
+	System.out.println(distance(e1, e3));
+	System.out.println(distance(e2, e3));
   }
 }
