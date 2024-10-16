b492d32aee12abccea969088b2f229abbd136117
Alisherjon Turakulov <aturakulov100@gmail.com>
2024-10-15 23:08:15 -0400

    added more tests cases for distance methods, they funciton.

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -15,10 +15,18 @@ public class Demo{
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
     Point p4 = new Point(p1);
-    System.out.println(p4);
+	//print using the toString() method we defined in Point.java
     System.out.println(p3);
-    System.out.println( distance(p1,p2));
+	System.out.println(p4); //should be same as p1
+	//corresponding tests for each dist mathod should be equal
+    System.out.println( distance(p1,p2)); 
+	System.out.println( distance(p1, p3));
+	System.out.println( distance(p2, p3));
     System.out.println( Point.distance(p1,p2));
+	System.out.println( Point.distance(p1, p3));
+	System.out.println( Point.distance(p2, p3));
     System.out.println( p1.distanceTo(p2));
+	System.out.println( p1.distanceTo(p3));
+	System.out.println( p2.distanceTo(p3));
   }
 }
