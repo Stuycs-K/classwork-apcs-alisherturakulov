4a2cf16adbd70a5bb4b1eee31177b9b03bd7bf45
Alisherjon Turakulov <aturakulov100@gmail.com>
2024-11-02 18:37:57 -0400

    create and set up tri tester and input file

---
+++ 10-31-Advent/TriangleTester.java
@@ -0,0 +1,22 @@
+//need import to use these objects
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class TriangleTester{
+	public static void main(String[] args){	
+		
+	}
+	
+	public static int countTrianglesA(String filename){
+		
+	}
+	
+	public static boolean isTriangle(String line){
+		
+	}
+	
+	public static int countTrianglesB(String filename){
+		
+	}
+}
