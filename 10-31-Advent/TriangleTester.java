27c3be4ceb62da1e3cbf55fac112417a79c50261
Alisherjon Turakulov <aturakulov100@gmail.com>
2024-11-02 23:48:21 -0400

    test countTrisB and verify functionality, add one more line to inputA making 6 lines total

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -6,6 +6,11 @@ import java.io.FileNotFoundException;
 public class TriangleTester{
 	public static void main(String[] args){
 		System.out.println(countTrianglesA("inputA.txt") + ": should be 2");
+		System.out.println(countTrianglesA("inputTri.txt") + ": countA with inputTri, is probably right");
+		
+		System.out.println("Tests for CountTrianglesB with inputA and inputTri------");
+		System.out.println(countTrianglesB("inputA.txt") + ": should be 1");
+		System.out.println(countTrianglesB("inputTri.txt") + ": is probably right, trust me pls");
 		
 	}
 	
@@ -37,25 +42,18 @@ public class TriangleTester{
 	public static int countTrianglesB(String filename){
 		try{
 			int count = 0;
-			File file = new File(fileName);
+			File file = new File(filename);
 			Scanner input =  new Scanner(file);
 			while(input.hasNextLine()) {
-				input.nextLine();
 				int a1 = input.nextInt(), b1 = input.nextInt(), c1 = input.nextInt();
-				input.nextLine();
 				int a2 = input.nextInt(), b2 = input.nextInt(), c2 = input.nextInt();
-				input.nextLine();
 				int a3 = input.nextInt(), b3 = input.nextInt(), c3 = input.nextInt();
-				if(isTriangle(a1, a2, a3){
-					count++;
-				}else if(isTriangle(b1, b2, b3)){
-					count++;
-				}else if(isTriangle(c1, c2, c3)){
-					count++;
-				}
+				if(isTriangle(a1, a2, a3)) count++;
+				if(isTriangle(b1, b2, b3)) count++;
+				if(isTriangle(c1, c2, c3)) count++;
 			}
 			return count;
-		}catch(){
+		}catch(FileNotFoundException e){
 			System.out.println("File not found");
 			return 0;
 		}
