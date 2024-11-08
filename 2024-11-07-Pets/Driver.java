a529d4d50237790cd240ee8f2c8af88d8d2dcd6a
Alisherjon Turakulov <aturakulov100@gmail.com>
2024-11-07 23:37:55 -0500

    test assignment of diff types, comment takeaways, test speak() methods on each type

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,6 +1,9 @@
-/*  Overriding the getName method in the Bird subclass, the speak method called the redefined
- getName from the Bird class.
-*/
+/*  Overriding the getName method in the Bird subclass, 
+    the speak method called the redefined getName from the Bird class.
+ 
+    Only assigning an Animal type to a Bird type did not work. This is because although 
+	Bird is-a Animal and Bird can use the same methods as Animal, Animal cant be assigned 
+	to the Bird type since the Bird would end up with undefined fields that the Animal class doesnt have.*/
 
 public class Driver{
     public static void main(String[] args){
@@ -10,5 +13,15 @@ public class Driver{
       System.out.println("--------");
       Bird bluejay = new Bird("chirp", 3, "bob", 2.4, "blue");
       bluejay.speak();
+	  System.out.println("\nType Assignment Tests");
+	  Animal a1 = new Animal("ahhk", 3, "Trixy");
+	  Animal a2 = new Bird("squee", 4, "sheldon", 4.5, "mahogany");
+	  Bird b1 = new Bird("quack", 5, "donald", 48.5, "green");
+	  // Bird b2 = new Animal("nya", 1, "Simba"); incompatible types error
+	  a1.speak();
+	  System.out.println("--------");
+	  a2.speak();
+	  System.out.println("--------");
+	  b1.speak();
     }
 }
