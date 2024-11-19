36744b53ed0306847ee8bb1a9fd6b0833aa3ff38
Alisherjon Turakulov <aturakulov100@gmail.com>
2024-11-18 23:02:18 -0500

    write method for day1.1, test fail

--- 2024-11-18-advent2016/ShortDistance.java
+++ 2024-11-18-advent2016/ShortDistance.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+//Day1 of 2016 Advent of Code TAXICAB geometry
+
 public class ShortDistance{
   public static void main(String[] args){
     System.out.println(shortestDist("directionsA.txt"));
@@ -14,11 +16,14 @@ public class ShortDistance{
       int x = 0, y = 0; // initial coordiinate points
 	  char[] dirs = new char[]{'N', 'E', 'S', 'W'}; //compass directions for determining add/subtraction x/y
 	  int dirIndex = 0;
+	  char turn; //stores R or L
+	  int blocks; // stores movement amt
+	  int taxiCabDistance; //taxicab geometry https://en.wikipedia.org/wiki/Taxicab_geometry
 	  
 	  while(input.hasNext()){
 	    String directions = input.next();
-		char turn = directions.charAt(0);
-		int blocks = Integer.parseInt(directions.substring(1,2));
+		turn = directions.charAt(0);
+		blocks = Integer.parseInt(directions.substring(1,2));
 		//to change facing direction
 		if(dirs[dirIndex] == 'N' &&  turn == 'L'){
 	      dirIndex = 3;
@@ -39,11 +44,15 @@ public class ShortDistance{
 		}else if(dirIndex == 3){
 		  x -= blocks;
 		}
+		//System.out.println(x + ", " + y + " with dirIndex NESW: " + dirIndex + " blocks moved: " + blocks);
 	  }
+	  
+	  taxiCabDistance = Math.abs(x) + Math.abs(y);
+	  return taxiCabDistance;
 	  
 	}catch(FileNotFoundException e){
 	  System.out.println("File not Found");
+	  return -1;
 	}
-    
   }
 }
