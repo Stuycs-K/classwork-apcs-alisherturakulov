14492aa3897985e488ca087d35b009d5d99e7ede
Alisherjon Turakulov <aturakulov100@gmail.com>
2024-11-19 23:26:45 -0500

    write and test retreiving letter count in name for each check sum

--- 2024-11-18-advent2016/Day4.java
+++ 2024-11-18-advent2016/Day4.java
@@ -2,8 +2,19 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 public class Day4{	
+
+	public static int countLetterInString(String name, char letter){
+	  int count = 0;
+	  for(int i = 0; i < name.length(); i++){
+	    if(name.charAt(i) == letter){
+		  count++;
+	    }
+	  }
+	  return count;
+	}
 	
 	public static int sumRealRooms(String filename){
 		try{
@@ -25,7 +36,15 @@ public class Day4{
 			sectorCheck = parts[parts.length-1];
 			sectorID = Integer.parseInt(sectorCheck.substring(0, sectorCheck.indexOf("[")));
 			checkSum = sectorCheck.substring(sectorCheck.indexOf("[")+1, sectorCheck.length()-1);
-		    System.out.println("name: " + name + " sectID: " + sectorID + " checkSum: " + checkSum);
+		    //System.out.println("name: " + name + " sectID: " + sectorID + " checkSum: " + checkSum);
+			int maxSum;
+			int sum;
+			int[] letterCounts = new int[checkSum.length()];
+			for(int i = 0; i < checkSum.length(); i++){
+			  letterCounts[i] = countLetterInString(name, checkSum.charAt(i));
+			}
+			System.out.println(Arrays.toString(letterCounts));
+			
 		  }
 		  return -1;
 		}catch(FileNotFoundException e){
