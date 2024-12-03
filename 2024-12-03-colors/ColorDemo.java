84b2f20da678f750885219e8a8f2ffc7cfda89e7
Alisherjon Turakulov <aturakulov100@gmail.com>
2024-12-03 18:41:48 -0500

    used methods go and color to print with different customizations

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,7 +1,12 @@
 public class ColorDemo{
     public static void main(String[] args){
       go(1,1);
-      color(31, 33);
+      color(31, 34);
+	  for(int i = 0; i < 10; i++){
+		  System.out.println("---->" + i);
+	  }
+	  go(24, 80);
+	  System.out.println("Hello");
       System.out.print("\u001b[0m");
     }
 
@@ -13,14 +18,14 @@ public class ColorDemo{
     }
 
     public static void go(int r, int c){
-      System.out.println("\u001b[" + r + ";" + c + "f");
+      System.out.print("\u001b[" + r + ";" + c + "f");
     }
 
     public static void color(int foreground,int background){
       System.out.print( "\u001b[" + foreground + ";" + (10+background) + "m");
     }
 
-    public static Void color(int foreground, int background, int modifier){
+    public static void color(int foreground, int background, int modifier){
       System.out.print( "\u001b[" + foreground + ";" + (10+background) + ";" + modifier + "m");
     }
 
