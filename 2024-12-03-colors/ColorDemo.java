2355705f09fe4b25193176659accdeac1243baf2
Alisherjon Turakulov <aturakulov100@gmail.com>
2024-12-03 20:52:26 -0500

    finished colorDemo pyramid with percentage and random colors

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,12 +1,26 @@
 public class ColorDemo{
     public static void main(String[] args){
-      go(1,1);
+	  go(1,1);
+	  System.out.println("\u001b[2J");
       color(31, 34);
-	  for(int i = 0; i < 10; i++){
-		  System.out.println("---->" + i);
+	  int foreground;
+	  int background;
+	  int modifier;
+	  for(int i = 1; i < 25; i++){
+		 
+		  for(int j = 0; j < i; j++){
+			System.out.print("\u001b[" + i + ";" + j + "f");
+			foreground = ((int) (Math.random()*9)) + 30;
+		    background = ((int) (Math.random()*9)) + 40;
+		    modifier = 4;
+			sleep(10);
+			System.out.print("\u001b[" + foreground + ";" + background + ";" + modifier + "m");
+		    System.out.print(" for:"+ foreground + " back:" + background + " mod:" + modifier);
+		  }
+		  System.out.println("\u001b[0m\u001b[0J\u001b[6m" + "Executing..." + (i*100)/25 + "%");
 	  }
-	  go(24, 80);
-	  System.out.println("Hello");
+	  go(24, 75);
+	  System.out.println("Done");
       System.out.print("\u001b[0m");
     }
 
@@ -22,11 +36,11 @@ public class ColorDemo{
     }
 
     public static void color(int foreground,int background){
-      System.out.print( "\u001b[" + foreground + ";" + (10+background) + "m");
+      System.out.print( "\u001b[" + foreground + ";" + (background) + "m");
     }
 
     public static void color(int foreground, int background, int modifier){
-      System.out.print( "\u001b[" + foreground + ";" + (10+background) + ";" + modifier + "m");
+      System.out.print( "\u001b[" + foreground + ";" + (background) + ";" + modifier + "m");
     }
 
 }
