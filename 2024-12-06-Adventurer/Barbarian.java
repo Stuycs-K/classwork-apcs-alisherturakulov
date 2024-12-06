e84e6e6b6aac902c42555876c0173eae19e2ff86
Alisherjon Turakulov <aturakulov100@gmail.com>
2024-12-06 10:55:01 -0500

    add more tests for barbarian in Driver, modify return strings for action methods

--- 2024-12-06-Adventurer/Barbarian.java
+++ 2024-12-06-Adventurer/Barbarian.java
@@ -1,25 +1,25 @@
 public class Barbarian extends Adventurer{
 	private int special, specialMax;
 	private String specialName = "Rage";
-	
+
 	//Constructor
 	public Barbarian(String name, int hp, int special){
 		super(name, hp);
 		this.special = special;
 		this.specialMax = special;
 	}
-	
+
 	public Barbarian(String name, int hp){
 		this(name, hp, hp);
 	}
-	
+
 	public Barbarian(String name){
 		super(name);
 		this.special = 10;
 		this.specialMax = 10;
 	}
-	
-	
+
+
 	//Methods
 	public String getSpecialName(){
 		return specialName;
@@ -28,18 +28,18 @@ public class Barbarian extends Adventurer{
 	public int getSpecial(){
 		return special;
 	}
-	
+
 	public void setSpecial(int n){
 		if(n > getSpecialMax()){
 			n = getSpecialMax();
 		}
 		this.special = n;
 	}
-	
+
 	public int getSpecialMax(){
 		return specialMax;
 	}
-	
+
 	/*
     all adventurers must have a way to attack enemies and
     support their allys
@@ -52,13 +52,7 @@ public class Barbarian extends Adventurer{
 
 	//heall or buff the target adventurer
 	public String support(Adventurer other){
-		int add = other.getHP()+10;
-		if(add > other.getmaxHP()){
-			other.setHP(add);
-			return "Buffed " + other.getName() + " 10 hp";
-		}
-		other.setHP(add);
-		return "Healed " + other.getName() + " 10 HP";
+		return other.support() + " by " + this.getName();
 	}
 
 	//heall or buff self
@@ -66,29 +60,27 @@ public class Barbarian extends Adventurer{
 		int add = getHP() + 10;
 		if(getmaxHP() < add){
 			setHP(add);
-			return "Buffed self 10 HP";
+			return this.getName() + " buffed 10 HP";
 		}
 		setHP(add);
-		return "Healed self 10 HP";
+		return this.getName() + " healed 10 HP";
 	}
 
 	//hurt or hinder the target adventurer, consume some special resource
 	public String specialAttack(Adventurer other){
 		int result = other.getHP() - 20;
 		other.setHP(result);
-			
-		
+
 		result = this.getSpecial() - 15;
 		if(result < 0){
 			result = 0;
 		}
 		this.setSpecial(result);
-		
-		return "Used 15 " + this.getSpecialName() + " to hurt " + other.getName() + " by 20"; 
+		return this.getName() + " Used 15 " + this.getSpecialName() + " to hurt " + other.getName() + " by 20";
 	}
-	
 
-	
-	
-	
+
+
+
+
 }
--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,17 +1,24 @@
 public class Driver{
-	
+
 	public static void main(String[] args){
 		Adventurer a1 = new Barbarian("Bob", 100, 200);
 		Adventurer a2 = new Barbarian("Joe", 50, 400);
 		a1.setName("Bane");
 		a1.setHP(10);
 		a1.setSpecial(60);
-		a1.applyDamage(10);
-		a1.support();
-		a1.support(a2);
-		a1.specialAttack(a2);
+		System.out.println(a1 + " HP" + a1.getHP());
+		System.out.println(a2 + " HP" + a2.getHP());
+		System.out.println(a1.attack(a2));
+		System.out.println(a1.support());
 		System.out.println(a1 + " HP" + a1.getHP() + " maxHP" + a1.getmaxHP());
-		System.out.println(a1 + " special" + a1.getSpecial() + " specialMax" + a1.getSpecialMax());
+		System.out.println(a1.support(a2));
+		System.out.println(a2 + " HP" + a2.getHP() + " Special: " + a2.getSpecialName());
+		System.out.println(a1.specialAttack(a2));
+		System.out.println(a1.support(a2));
+		System.out.println(a1 + " restore special by " + a1.restoreSpecial(14));
+		System.out.println(a1 + " HP" + a1.getHP() + " maxHP" + a1.getmaxHP());
+		System.out.println(a1 + " special:" + a1.getSpecial() + " specialMax" + a1.getSpecialMax());
 		System.out.println(a2 + " HP" + a2.getHP() + " specialMax" + a2.getSpecialMax());
+
 	}
 }
