cc75c38ef1a3bf2334ae58724c412b86073251ac
Alisherjon Turakulov <aturakulov100@gmail.com>
2024-12-09 11:00:58 -0500

    write firs ttwo steps, begin user input

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
 public class Game{
-  
+  public static void main(String[] args){
+    Barbarian enemy = new Barbarian("barb", 80, 45);
+    CodeWarrior player = new CodeWarrior("Joe", 120, "Rust");
+      //while(true){
+      System.out.println("Player " +player + ", " + player.getHP() + "/" + player.getmaxHP() +" HP," + player.getSpecial() + "/" + player.getSpecialMax() + " " + player.getSpecialName());
+      System.out.println("Enemy " + enemy + ", " + enemy.getHP() + "/" + enemy.getmaxHP() +" HP," + enemy.getSpecial() + "/" + enemy.getSpecialMax() + " " + enemy.getSpecialName());
+      System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+      Scanner userIn = new Scanner(System.in);
+  //}
+}
 }
