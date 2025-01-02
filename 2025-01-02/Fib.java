1c0879c6ca98533e0d14ea0d75f299ca31f70df8
Alisherjon Turakulov <aturakulov100@gmail.com>
2025-01-02 10:58:08 -0500

    write and test Fib for under ten seconds and max before wrapping

--- 2025-01-02/Fib.java
+++ 2025-01-02/Fib.java
@@ -1,9 +1,18 @@
 public class Fib{
   public static void main(String[] args){
-
+  /* System.out.println(fib(0));
+   System.out.println(fib(1));
+   System.out.println(fib(2));
+   System.out.println(fib(3));*/
+   System.out.println(fib(46)); //largest that takes lless than 100 seconds
+   // about 2.1 billion max possible
   }
 
   public static int fib(int n){
-    
+    if(n == 1 || n == 0){
+      return n;
+    }else{
+      return fib(n-2) + fib(n-1);
+    }
   }
 }
