4422742bd7a0c41dfc7636021219e5889595ece3
Alisherjon Turakulov <aturakulov100@gmail.com>
2025-01-03 10:56:33 -0500

    create makeWords.java and start writing mw

---
+++ 2025-01-02/MakeWords.java
@@ -0,0 +1,15 @@
+public class MakeWords{
+  public static void main(sttring[] args){
+
+  }
+
+  public static void makeWords(int remainingLetters, String result, String alphabet){
+  if(remainingLetters == 0){
+    System.out.println(result);
+  }else{
+    
+
+  }
+
+  }
+}
